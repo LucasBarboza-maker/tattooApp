185a873529176f9ad8c46c4c8d9147bd343c62a5
Lucas R Barboza <lucasrb18@gmail.com>
2021-01-03 13:18:51 -0300

    Adding routes to frontEnd of the project

---
+++ frontEnd/Screens/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, ScrollView, TextInput, Text} from 'react-native'
+
+
+export default function App(){
+    return(
+        <ScrollView>
+            <View>
+                <Text>
+                    Ola mundao
+                </Text>
+            </View>
+        </ScrollView>
+    )
+}
